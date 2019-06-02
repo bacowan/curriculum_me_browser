42a5bbe311d60666825f571fd425bbac8d445d5f
Brendan Cowan <bacowan@ualberta.ca>
2019-06-02 14:29:03 -0300

    making navbar sticky

--- src/MainNavbar.jsx
+++ src/MainNavbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import HelloWorld from './HelloWorld.jsx';
 
 class MainNavbar extends React.Component {
     render() { return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" fixed="top" sticky="top">
             <Navbar.Brand>Curriculum.me</Navbar.Brand>
         </Navbar>
     ) }
